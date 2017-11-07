ee54b0b1b1ce69d8a7a0da7fe727891feed3f309
Trevor Sandy <trevor.sandy@gmail.com>
2017-11-07 05:23:49 +0100

    AppVeyor debug 10 [deploy pkg] [skip travis]

--- r.bat
+++
@@ -1,9 +0,0 @@
-SET POV_BUILD_PKG=yes
-IF /I "%POV_BUILD_PKG%" EQU "yes" (
-	CD windows\vs2015
-	autobuild -allcui -allins
-) ELSE (
-	CD windows\vs2015
-	autobuild x86 -chk
-	autobuild x86_64 -chk
-)
